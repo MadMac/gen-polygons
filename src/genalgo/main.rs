bcb31cbfc88c92e95750af9c12cf9fff909b305e
Markus Hällfors <markus@hallfors.com>
2021-06-12 10:39:05 +0300

    Some init work on gen algo

--- src/genalgo/main.rs
+++ src/genalgo/main.rs
@@ -1,5 +1,21 @@
 use oxigen::prelude::*;
 
+#[derive(Clone)]
+struct Vertex {
+    position: [f32; 3],
+    color: [f32; 4],
+}
+
+#[derive(Clone)]
+struct Picture {
+	vertices: [Vertex; 20]
+}
+
 fn main() {
+	let problem_size = 60; // Has to be divisible by 3
+	let population_size = 2_i32.pow(problem_size as u32) as usize;
+	let (solutions, generation, _progress, _population) = GeneticExecution::<bool, Picture>::new()
+		.population_size()
+		.run();
 	println!("Running genalgo...");
 }
